557b3bf68fa2b110879364387aad6945311126a7
HardRandom <90847034+HardRandom@users.noreply.github.com>
2025-05-16 17:32:26 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,6 +1,5 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
-#include <stdexcept>
 
 Train::Train() : countOp(0), first(nullptr) {}
 Train::~Train() {
@@ -22,8 +21,8 @@ void Train::addCar(bool light) {
          Car* last = first->prev;
          last->next = newCar;
          newCar->prev = last;
-          newCar->next = first;
-          first->prev = newCar;
+         newCar->next = first;
+         first->prev = newCar;
        }
  }
 int Train::getLength() {
